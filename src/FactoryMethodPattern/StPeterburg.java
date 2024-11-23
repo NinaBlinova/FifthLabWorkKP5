a9bb08d2687f62895210c4e7722ac7279b979320
Blinova Nina <blinovan1805@gmail.com>
2024-11-23 14:14:09 +0300

    Реализация паттерна закончена

--- src/FactoryMethodPattern/StPeterburg.java
+++ src/FactoryMethodPattern/StPeterburg.java
@@ -1,18 +1,42 @@
 package FactoryMethodPattern;
 
-public class StPeterburg extends DutyFree{
+public class StPeterburg extends DutyFree {
+
+    private double cashAvailable; // Наличные средства
+    private double cardBalance;    // Деньги на карте
+    private double ticketPrice;    // Цена билета
+    LoyaltyProgram loyaltyProgram = null;
+    public String airportName;    // Название аэропорта
+
+    public StPeterburg(double cashAvailable, double cardBalance, double ticketPrice, String airportName) {
+        this.cashAvailable = cashAvailable;
+        this.cardBalance = cardBalance;
+        this.ticketPrice = ticketPrice;
+        this.airportName = airportName;
+    }
+
+
+    public void create(String airportName) {
+        // Сравниваем строку с названиями аэропортов
+        if (airportName.equalsIgnoreCase("PulkovoA")) {
+            loyaltyProgram = new Domodedovo(cashAvailable, cardBalance, ticketPrice);
+        } else if (airportName.equalsIgnoreCase("PulkovoB")) {
+            loyaltyProgram = new Sheremetyevo(cashAvailable, cardBalance, ticketPrice);
+        }
+    }
+
     @Override
     public double getDiscount() {
-        return 0;
+        return loyaltyProgram.calculateDiscount();
     }
 
     @Override
     public int getBonusPoints() {
-        return 0;
+        return loyaltyProgram.calculateBonusPoints();
     }
 
     @Override
     public String getConditions() {
-        return "";
+        return loyaltyProgram.getConditions();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,49 @@
 import FactoryMethodPattern.Domodedovo;
 import FactoryMethodPattern.Moskow;
+import FactoryMethodPattern.StPeterburg;
 
 public class Main {
     public static void main(String[] args) {
         Moskow moskowAirport = new Moskow(10000, 7000, 100000, "Sheremetyevo");
         moskowAirport.create(moskowAirport.airportName);
-        System.out.println(moskowAirport.getDiscount());
-        System.out.println(moskowAirport.getBonusPoints());
+        System.out.println("Аэропорт: " + moskowAirport.airportName);
+        System.out.println("Скидка: " + moskowAirport.getDiscount());
+        System.out.println("Бонусы: " + moskowAirport.getBonusPoints());
         System.out.println(moskowAirport.getConditions());
+        System.out.println("--------------------------");
+
+        Moskow moskowAirport2 = new Moskow(33000, 2000, 40000, "Domodedovo");
+        moskowAirport2.create(moskowAirport2.airportName);
+        System.out.println("Аэропорт: " + moskowAirport2.airportName);
+        System.out.println("Скидка: " + moskowAirport2.getDiscount());
+        System.out.println("Бонусы: " + moskowAirport2.getBonusPoints());
+        System.out.println(moskowAirport2.getConditions());
+        System.out.println("--------------------------");
+
+
+        Moskow moskowAirport3 = new Moskow(15000, 44000, 78000, "Vnukovo");
+        moskowAirport3.create(moskowAirport3.airportName);
+        System.out.println("Аэропорт: " + moskowAirport3.airportName);
+        System.out.println("Скидка: " + moskowAirport3.getDiscount());
+        System.out.println("Бонусы: " + moskowAirport3.getBonusPoints());
+        System.out.println(moskowAirport3.getConditions());
+        System.out.println("--------------------------");
+
+        StPeterburg stPeterburg = new StPeterburg(11000, 7000, 66000, "PulkovoA");
+        stPeterburg.create(stPeterburg.airportName);
+        System.out.println("Аэропорт: " + stPeterburg.airportName);
+        System.out.println("Скидка: " + stPeterburg.getDiscount());
+        System.out.println("Бонусы: " + stPeterburg.getBonusPoints());
+        System.out.println(stPeterburg.getConditions());
+        System.out.println("--------------------------");
+
+
+        StPeterburg stPeterburg2 = new StPeterburg(50000, 5000, 11000, "PulkovoB");
+        stPeterburg2.create(stPeterburg2.airportName);
+        System.out.println("Аэропорт: " + stPeterburg2.airportName);
+        System.out.println("Скидка: " + stPeterburg2.getDiscount());
+        System.out.println("Бонусы: " + stPeterburg2.getBonusPoints());
+        System.out.println(stPeterburg2.getConditions());
+        System.out.println("--------------------------");
     }
 }
